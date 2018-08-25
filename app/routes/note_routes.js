427b5276759aed904e922245aea0e9ac1f4f8c91
filippopiconese <filippopiconese@hotmail.it>
2018-08-25 12:45:20 +0200

    feat(delete-method): created the delete-method in order to remove a record from a database

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -13,6 +13,18 @@ module.exports = function (app, db) {
     })
   })
 
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id
+
+    const details = { '_id': new ObjectID(id) }
+
+    db.collection('notes').remove(details, (err, item) => {
+      err
+        ? res.send({ 'error': 'An error has occurred on DELETE' })
+        : res.send('Note ' + id + ' deleted!')
+    })
+  })
+
   app.post('/notes', (req, res) => {
     const note = { text: req.body.Body, title: req.body.Title }
 
