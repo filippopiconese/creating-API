b762de29d31b622ff23b4a58fbd43ca1bfae061f
filippopiconese <filippopiconese@hotmail.it>
2018-08-25 10:55:55 +0200

    feat(post-method postman): Now we can see how the post-method works using Postman app

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-const port = 8000
+app.use(bodyParser.urlencoded({ extended: true }))
 
 require('./app/routes')(app, {})
+
+const port = 8000
 
 app.listen(port, () => {
   console.log(`We are live on ${port}`)
