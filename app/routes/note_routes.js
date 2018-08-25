5ac4a9425a106289bc6dec0c19a59e63454b0000
filippopiconese <filippopiconese@hotmail.it>
2018-08-25 12:39:51 +0200

    feat(get-method): Created the get method in order to read a record into a collection

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,10 +1,24 @@
+var ObjectID = require('mongodb').ObjectID
+
 module.exports = function (app, db) {
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id // it's used to GET the ID from the URL
+
+    const details = { '_id': new ObjectID(id) }
+
+    db.collection('notes').findOne(details, (err, item) => {
+      err
+        ? res.send({ 'error': 'An error has occurred on GET' })
+        : res.send(item)
+    })
+  })
+
   app.post('/notes', (req, res) => {
     const note = { text: req.body.Body, title: req.body.Title }
 
     db.collection('notes').insert(note, (err, result) => {
       err
-        ? res.send({ 'error': 'An error has occurred' })
+        ? res.send({ 'error': 'An error has occurred on POST' })
         : res.send(result.ops[0])
     })
   })
