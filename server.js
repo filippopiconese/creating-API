6550967eacbaeebd4b6fe1c0ec48778196ce50f4
filippopiconese <filippopiconese@hotmail.it>
2018-08-25 11:42:47 +0200

    feat(DB-setup): Set up of MongoDB requirements in order to use the free mLab DB created on this purp

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const MongoClient = require('mongodb').MongoClient
 const bodyParser = require('body-parser')
+const db = require('./config/db')
 
 const app = express()
 
@@ -10,6 +11,13 @@ require('./app/routes')(app, {})
 
 const port = 8000
 
-app.listen(port, () => {
-  console.log(`We are live on ${port}`)
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.error(err)
+
+  const db = database.db('filippo3000database')
+  require('./app/routes')(app, db)
+
+  app.listen(port, () => {
+    console.log('We are live on ' + port)
+  })
 })
